de5471212577a947e32dd999224915959ff9756f
Shahriar Bourbour <Owner@Owner-PC>
2020-01-27 22:33:41 -0600

    Added elasticsearch querybody.

--- ElasticSearchQuery.py
+++ ElasticSearchQuery.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from elasticsearch import Elasticsearch 
 
 def getData(searchTerm):
     url = "http://localhost:9200/books/_search?q=" + searchTerm    
@@ -11,5 +12,26 @@ searchTerm = input('Please enter search term:')
 responseContent = getData(searchTerm)
 
 for entry in responseContent['hits']['hits']:    
+    print(entry['_source'])
+    print()
+    
+searchField = input('Please enter search field:')
+searchTerm = input('Please enter search term:')
+    
+query_body = {
+  "query": {
+    "bool": {
+      "must": {
+        "match": {      
+          searchField: searchTerm
+        }
+      }
+    }
+  }
+}
+
+elastic_client = Elasticsearch(hosts=["http://localhost:9200"])
+result = elastic_client.search(index="books", body=query_body)
+for entry in result['hits']['hits']:    
     print(entry['_source'])
     print()
